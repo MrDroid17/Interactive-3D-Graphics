d282be1ec84f52b5581b5dd4c9b156cdee617276
Sobhit Kumar <sobhit2010@gmail.com>
2019-10-14 00:06:21 +0530

    Setting Pivot

--- unit4/unit4-clock_exercise.js
+++ unit4/unit4-clock_exercise.js
@@ -15,79 +15,89 @@ var ground = true;
 
 function fillScene() {
 	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog( 0x808080, 2000, 4000 );
+	scene.fog = new THREE.Fog(0x808080, 2000, 4000);
 
 	// LIGHTS
-	var ambientLight = new THREE.AmbientLight( 0x222222 );
+	var ambientLight = new THREE.AmbientLight(0x222222);
 
-	var light = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
-	light.position.set( 200, 400, 500 );
+	var light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	light.position.set(200, 400, 500);
 
-	var light2 = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
-	light2.position.set( -500, 250, -200 );
+	var light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	light2.position.set(-500, 250, -200);
 
 	scene.add(ambientLight);
 	scene.add(light);
 	scene.add(light2);
 
-	var faceMaterial = new THREE.MeshLambertMaterial( { color: 0xFFECA9 } );
-	var markMaterial = new THREE.MeshLambertMaterial( { color: 0x89581F } );
-	var mark12Material = new THREE.MeshLambertMaterial( { color: 0xE6880E } );
-	var minuteHandMaterial = new THREE.MeshLambertMaterial( { color: 0x226894 } );
-	var hourHandMaterial = new THREE.MeshLambertMaterial( { color: 0xE02BFB } );
+	var faceMaterial = new THREE.MeshLambertMaterial({ color: 0xFFECA9 });
+	var markMaterial = new THREE.MeshLambertMaterial({ color: 0x89581F });
+	var mark12Material = new THREE.MeshLambertMaterial({ color: 0xE6880E });
+	var minuteHandMaterial = new THREE.MeshLambertMaterial({ color: 0x226894 });
+	var hourHandMaterial = new THREE.MeshLambertMaterial({ color: 0xE02BFB });
 
 	// clock
 	var clock = new THREE.Mesh(
-		new THREE.CylinderGeometry( 75, 75, 10, 32 ), faceMaterial );
-		//new THREE.CubeGeometry( 150, 5, 150 ), faceMaterial );
+		new THREE.CylinderGeometry(75, 75, 10, 32), faceMaterial);
+	//new THREE.CubeGeometry( 150, 5, 150 ), faceMaterial );
 	clock.position.y = 5;
-	scene.add( clock );
+	scene.add(clock);
 
 	// marks
 	var cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 20, 4, 15 ), mark12Material );
+		new THREE.CubeGeometry(20, 4, 15), mark12Material);
 	cube.position.x = 60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.x = -60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.z = 60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.z = -60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
-  // YOUR CODE HERE:
-  // The dimensions and rotation angles of the hands are correct
-  // you just have find a way to perform them in the correct order
+	// YOUR CODE HERE:
+	// The dimensions and rotation angles of the hands are correct
+	// you just have find a way to perform them in the correct order
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 70, 4, 4 ), minuteHandMaterial );
+		new THREE.CubeGeometry(70, 4, 4), minuteHandMaterial);
 	cube.position.y = 14;
-	cube.position.x = 70/2 - 10;
-	cube.rotation.y = -60 * Math.PI/180;
-	scene.add( cube );
+	cube.position.x = 70 / 2 - 10;
+	cube.rotation.y = -60 * Math.PI / 180;
+	scene.add(cube);
+
+	var minuteHand = new THREE.Object3D();
+	minuteHand.add(cube);
+
+	minuteHand.rotation.y = -60 * Math.PI / 180;
+	scene.add(minuteHand);
 
 	var sphere = new THREE.Mesh(
-		new THREE.SphereGeometry( 0.5, 32, 16 ), hourHandMaterial );
+		new THREE.SphereGeometry(0.5, 32, 16), hourHandMaterial);
 	sphere.position.y = 18;	// move the hand above the other hand
-	sphere.position.x = 50/2 - 10;
-	sphere.rotation.y = 30 * Math.PI/180;
+	sphere.position.x = 45 / 2 - 10;
+	sphere.rotation.y = 30 * Math.PI / 180;
 	sphere.scale.x = 50;
 	sphere.scale.y = 4;
 	sphere.scale.z = 4;
+	var hourHand = new THREE.Object3D();
+	hourHand.add(sphere);
 
-	scene.add( sphere );
+	hourHand.rotation.y = 30 * Math.PI / 180;
+	scene.add(hourHand);
+	scene.add(sphere);
 }
 
 function init() {
@@ -99,47 +109,47 @@ function init() {
 	var canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
 	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColorHex( 0xAAAAAA, 1.0 );
+	renderer.setClearColorHex(0xAAAAAA, 1.0);
 
 	// CAMERA
-	camera = new THREE.PerspectiveCamera( 30, canvasRatio, 1, 10000 );
-	camera.position.set( -420, 400, 100 );
+	camera = new THREE.PerspectiveCamera(30, canvasRatio, 1, 10000);
+	camera.position.set(-420, 400, 100);
 	// CONTROLS
 	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
-	cameraControls.target.set(0,0,0);
+	cameraControls.target.set(0, 0, 0);
 
 	fillScene();
 
 }
 function drawHelpers() {
 	if (ground) {
-		Coordinates.drawGround({size:10000});
+		Coordinates.drawGround({ size: 10000 });
 	}
 	if (gridX) {
-		Coordinates.drawGrid({size:10000,scale:0.01});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01 });
 	}
 	if (gridY) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"y"});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01, orientation: "y" });
 	}
 	if (gridZ) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"z"});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01, orientation: "z" });
 	}
 	if (axes) {
-		Coordinates.drawAllAxes({axisLength:200,axisRadius:1,axisTess:50});
+		Coordinates.drawAllAxes({ axisLength: 200, axisRadius: 1, axisTess: 50 });
 	}
 }
 
 function addToDOM() {
 	var container = document.getElementById('container');
 	var canvas = container.getElementsByTagName('canvas');
-	if (canvas.length>0) {
+	if (canvas.length > 0) {
 		container.removeChild(canvas[0]);
 	}
-	container.appendChild( renderer.domElement );
+	container.appendChild(renderer.domElement);
 }
 
 function animate() {
@@ -151,8 +161,7 @@ function render() {
 	var delta = clock.getDelta();
 	cameraControls.update(delta);
 
-	if ( effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes)
-	{
+	if (effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes) {
 		gridX = effectController.newGridX;
 		gridY = effectController.newGridY;
 		gridZ = effectController.newGridZ;
@@ -179,11 +188,11 @@ function setupGui() {
 	};
 
 	var gui = new dat.GUI();
-	gui.add( effectController, "newGridX").name("Show XZ grid");
-	gui.add( effectController, "newGridY" ).name("Show YZ grid");
-	gui.add( effectController, "newGridZ" ).name("Show XY grid");
-	gui.add( effectController, "newGround" ).name("Show ground");
-	gui.add( effectController, "newAxes" ).name("Show axes");
+	gui.add(effectController, "newGridX").name("Show XZ grid");
+	gui.add(effectController, "newGridY").name("Show YZ grid");
+	gui.add(effectController, "newGridZ").name("Show XY grid");
+	gui.add(effectController, "newGround").name("Show ground");
+	gui.add(effectController, "newAxes").name("Show axes");
 }
 
 try {
@@ -192,8 +201,8 @@ try {
 	drawHelpers();
 	addToDOM();
 	animate();
-} catch(e) {
+} catch (e) {
 	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
-	$('#container').append(errorReport+e);
+	$('#container').append(errorReport + e);
 }
 
--- unit4/unit4-scale_exercise.js
+++ unit4/unit4-scale_exercise.js
@@ -15,90 +15,94 @@ var ground = true;
 
 function fillScene() {
 	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog( 0x808080, 2000, 4000 );
+	scene.fog = new THREE.Fog(0x808080, 2000, 4000);
 
 	// LIGHTS
-	var ambientLight = new THREE.AmbientLight( 0x222222 );
+	var ambientLight = new THREE.AmbientLight(0x222222);
 
-	var light = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
-	light.position.set( 200, 400, 500 );
+	var light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	light.position.set(200, 400, 500);
 
-	var light2 = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
-	light2.position.set( -500, 250, -200 );
+	var light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+	light2.position.set(-500, 250, -200);
 
 	scene.add(ambientLight);
 	scene.add(light);
 	scene.add(light2);
 
-	var faceMaterial = new THREE.MeshLambertMaterial( { color: 0xFFECA9 } );
-	var markMaterial = new THREE.MeshLambertMaterial( { color: 0x89581F } );
-	var mark12Material = new THREE.MeshLambertMaterial( { color: 0xE6880E } );
-	var minuteHandMaterial = new THREE.MeshLambertMaterial( { color: 0x226894 } );
-	var hourHandMaterial = new THREE.MeshLambertMaterial( { color: 0xE02BFB } );
+	var faceMaterial = new THREE.MeshLambertMaterial({ color: 0xFFECA9 });
+	var markMaterial = new THREE.MeshLambertMaterial({ color: 0x89581F });
+	var mark12Material = new THREE.MeshLambertMaterial({ color: 0xE6880E });
+	var minuteHandMaterial = new THREE.MeshLambertMaterial({ color: 0x226894 });
+	var hourHandMaterial = new THREE.MeshLambertMaterial({ color: 0xE02BFB });
 
 	// clock
 	var clock = new THREE.Mesh(
-		new THREE.CylinderGeometry( 75, 75, 10, 32 ), faceMaterial );
-		//new THREE.CubeGeometry( 150, 5, 150 ), faceMaterial );
+		new THREE.CylinderGeometry(75, 75, 10, 32), faceMaterial);
+	//new THREE.CubeGeometry( 150, 5, 150 ), faceMaterial );
 	clock.position.y = 5;
-	scene.add( clock );
+	scene.add(clock);
 
 	// marks
 	var cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 20, 4, 15 ), mark12Material );
+		new THREE.CubeGeometry(20, 4, 15), mark12Material);
 	cube.position.x = 60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.x = -60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.z = 60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 10, 4, 10 ), markMaterial );
+		new THREE.CubeGeometry(10, 4, 10), markMaterial);
 	cube.position.z = -60;
 	cube.position.y = 9;
-	scene.add( cube );
+	scene.add(cube);
 
 	cube = new THREE.Mesh(
-		new THREE.CubeGeometry( 110, 4, 4 ), minuteHandMaterial );
+		new THREE.CubeGeometry(110, 4, 4), minuteHandMaterial);
 	cube.position.y = 14;
-	cube.rotation.y = -60 * Math.PI/180;
+	cube.rotation.y = -60 * Math.PI / 180;
 
-	scene.add( cube );
+	scene.add(cube);
 
 	var sphere = new THREE.Mesh(
-		new THREE.SphereGeometry( 10, 32, 16 ), hourHandMaterial );
+		new THREE.SphereGeometry(10, 32, 16), hourHandMaterial);
 	sphere.position.y = 18;	// move the hand above the other hand
 
 	// YOUR CODE HERE:
+	sphere.scale.z = 0.2;
+	sphere.scale.y = 0.2;
+	sphere.scale.x = 4;
+	sphere.rotation.y = 60 + Math.PI / 180;
 
-	scene.add( sphere );
+	scene.add(sphere);
 }
 
 function drawHelpers() {
 	if (ground) {
-		Coordinates.drawGround({size:10000});
+		Coordinates.drawGround({ size: 10000 });
 	}
 	if (gridX) {
-		Coordinates.drawGrid({size:10000,scale:0.01});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01 });
 	}
 	if (gridY) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"y"});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01, orientation: "y" });
 	}
 	if (gridZ) {
-		Coordinates.drawGrid({size:10000,scale:0.01, orientation:"z"});
+		Coordinates.drawGrid({ size: 10000, scale: 0.01, orientation: "z" });
 	}
 	if (axes) {
-		Coordinates.drawAllAxes({axisLength:200,axisRadius:1,axisTess:50});
+		Coordinates.drawAllAxes({ axisLength: 200, axisRadius: 1, axisTess: 50 });
 	}
 }
 function init() {
@@ -110,18 +114,18 @@ function init() {
 	var canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
 	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColorHex( 0xAAAAAA, 1.0 );
+	renderer.setClearColorHex(0xAAAAAA, 1.0);
 
 	// CAMERA
-	camera = new THREE.PerspectiveCamera( 30, canvasRatio, 1, 10000 );
-	camera.position.set( -420, 400, 100 );
+	camera = new THREE.PerspectiveCamera(30, canvasRatio, 1, 10000);
+	camera.position.set(-420, 400, 100);
 	// CONTROLS
 	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
-	cameraControls.target.set(0,0,0);
+	cameraControls.target.set(0, 0, 0);
 
 	fillScene();
 
@@ -130,10 +134,10 @@ function init() {
 function addToDOM() {
 	var container = document.getElementById('container');
 	var canvas = container.getElementsByTagName('canvas');
-	if (canvas.length>0) {
+	if (canvas.length > 0) {
 		container.removeChild(canvas[0]);
 	}
-	container.appendChild( renderer.domElement );
+	container.appendChild(renderer.domElement);
 }
 
 function animate() {
@@ -145,8 +149,7 @@ function render() {
 	var delta = clock.getDelta();
 	cameraControls.update(delta);
 
-	if ( effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes)
-	{
+	if (effectController.newGridX !== gridX || effectController.newGridY !== gridY || effectController.newGridZ !== gridZ || effectController.newGround !== ground || effectController.newAxes !== axes) {
 		gridX = effectController.newGridX;
 		gridY = effectController.newGridY;
 		gridZ = effectController.newGridZ;
@@ -173,11 +176,11 @@ function setupGui() {
 	};
 
 	var gui = new dat.GUI();
-	gui.add( effectController, "newGridX").name("Show XZ grid");
-	gui.add( effectController, "newGridY" ).name("Show YZ grid");
-	gui.add( effectController, "newGridZ" ).name("Show XY grid");
-	gui.add( effectController, "newGround" ).name("Show ground");
-	gui.add( effectController, "newAxes" ).name("Show axes");
+	gui.add(effectController, "newGridX").name("Show XZ grid");
+	gui.add(effectController, "newGridY").name("Show YZ grid");
+	gui.add(effectController, "newGridZ").name("Show XY grid");
+	gui.add(effectController, "newGround").name("Show ground");
+	gui.add(effectController, "newAxes").name("Show axes");
 }
 
 try {
@@ -186,7 +189,7 @@ try {
 	drawHelpers();
 	addToDOM();
 	animate();
-} catch(e) {
+} catch (e) {
 	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
-	$('#container').append(errorReport+e);
+	$('#container').append(errorReport + e);
 }
